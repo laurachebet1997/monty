a92445989256bfaa6c0fa2c8694e9461e97507a9
laura <rosechebet1997@gmail.com>
2022-06-15 13:28:58 -0700

    added hheader with node structures

--- monty.h
+++ monty.h
@@ -1,12 +1,20 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdbool>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
@@ -21,7 +29,6 @@ typedef struct stack_s
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
